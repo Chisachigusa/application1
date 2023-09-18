985a7f0e0525d2c075465bf83e14bf720613f475
Chisachigusa <144024910+Chisachigusa@users.noreply.github.com>
2023-09-18 01:45:15 +0000

    commit_lesson6

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,7 +7,7 @@ class ListsController < ApplicationController
     list = List.new(list_params)
     
     list.save
-    redirect_to '/top'
+    redirect_to list_path(list.id)
   end
 
 
@@ -20,9 +20,23 @@ class ListsController < ApplicationController
   end
 
   def edit
-  end
+    @list = List.find(params[:id])
+  end  
+  
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)  
+  end  
+  
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to lists_path(list.id)
+  end  
   
   private
   def list_params
+    params.require(:list).permit(:title, :body, :image)
   end  
 end
